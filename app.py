5b48790c28d49228f18603f6a0e3cd5a48d48158
Konstantina Chrysochoidou <dinaxrisoxoidou@gmail.com>
2026-06-14 17:14:56 +0300

    formatted app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from datetime import datetime, timedelta
 app = Flask(__name__)
 app.secret_key = "library_system_secret_key"
 
+
 # συνδεση με τη βαση δεδομενων
 
 app.config["SQLALCHEMY_DATABASE_URI"] = \
@@ -15,6 +16,9 @@ app.config["SQLALCHEMY_DATABASE_URI"] = \
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
+
+
+# ενημερωση συνολικου υπολοιπου μελους
 
 def update_member_balance(member_id):
 
@@ -37,40 +41,73 @@ def update_member_balance(member_id):
     db.session.commit()
 
 
+#------------------- αρχικη -------------------
+
+
 # αρχικη σελιδα
 
 @app.route("/")
 def home():
 
-    return render_template("index.html")
+    return render_template(
+
+        "index.html"
+
+    )
 
 
 #------------------- βιβλια -------------------
 
+
 # προσθηκη βιβλιου
 
-@app.route("/add-book", methods=["GET", "POST"])
+@app.route(
+    "/add-book",
+    methods=["GET", "POST"]
+)
 def add_book():
 
     if request.method == "POST":
 
         # ελεγχος αν ανεβηκε εικονα
 
-        cover_file = request.files.get("cover_upload")
+        cover_file = request.files.get(
 
-        cover_url = request.form["cover_url"]
+            "cover_upload"
+
+        )
+
+        cover_url = request.form[
+
+            "cover_url"
+
+        ]
 
         if cover_file and cover_file.filename != "":
 
             filepath = os.path.join(
+
                 "static",
+
                 "uploads",
+
                 cover_file.filename
+
             )
 
-            cover_file.save(filepath)
+            cover_file.save(
 
-            cover_url = "/" + filepath.replace("\\", "/")
+                filepath
+
+            )
+
+            cover_url = "/" + filepath.replace(
+
+                "\\",
+
+                "/"
+
+            )
 
         # δημιουργια νεου βιβλιου
 
@@ -101,7 +138,13 @@ def add_book():
             description=request.form["description"],
 
             categories=",".join(
-                request.form.getlist("categories")
+
+                request.form.getlist(
+
+                    "categories"
+
+                )
+
             ),
 
             cover_url=cover_url,
@@ -112,13 +155,25 @@ def add_book():
 
         )
 
-        db.session.add(book)
+        db.session.add(
+
+            book
+
+        )
 
         db.session.commit()
 
-        return redirect("/books")
+        return redirect(
 
-    return render_template("add_book.html")
+            "/books"
+
+        )
+
+    return render_template(
+
+        "add_book.html"
+
+    )
 
 
 # λιστα βιβλιων και φιλτρα
@@ -126,18 +181,48 @@ def add_book():
 @app.route("/books")
 def books():
 
-    search = request.args.get("search", "")
+    search = request.args.get(
 
-    categories = request.args.getlist("categories")
+        "search",
 
-    language = request.args.get("language", "")
+        ""
 
-    year = request.args.get("year", "")
+    )
 
-    pages = request.args.get("pages", "")
+    categories = request.args.getlist(
+
+        "categories"
+
+    )
+
+    language = request.args.get(
+
+        "language",
+
+        ""
+
+    )
+
+    year = request.args.get(
+
+        "year",
+
+        ""
+
+    )
+
+    pages = request.args.get(
+
+        "pages",
+
+        ""
+
+    )
 
     available_only = request.args.get(
+
         "available_only"
+
     )
 
     # αρχικο query
@@ -149,8 +234,19 @@ def books():
     if search:
 
         query = query.filter(
-            (Book.title.ilike(f"%{search}%")) |
-            (Book.authors.ilike(f"%{search}%"))
+
+            (Book.title.ilike(
+
+                f"%{search}%"
+
+            )) |
+
+            (Book.authors.ilike(
+
+                f"%{search}%"
+
+            ))
+
         )
 
     # κατηγοριες
@@ -160,9 +256,13 @@ def books():
         for category in categories:
 
             query = query.filter(
+
                 Book.categories.ilike(
+
                     f"%{category}%"
+
                 )
+
             )
 
     # γλωσσα
@@ -170,7 +270,9 @@ def books():
     if language and language != "Όλες":
 
         query = query.filter(
+
             Book.language == language
+
         )
 
     # ετος
@@ -178,41 +280,57 @@ def books():
     if year == "Πριν το 1950":
 
         query = query.filter(
+
             Book.year < 1950
+
         )
 
     elif year == "1950-1979":
 
         query = query.filter(
+
             Book.year >= 1950,
+
             Book.year <= 1979
+
         )
 
     elif year == "1980-1999":
 
         query = query.filter(
+
             Book.year >= 1980,
+
             Book.year <= 1999
+
         )
 
     elif year == "2000-2009":
 
         query = query.filter(
+
             Book.year >= 2000,
+
             Book.year <= 2009
+
         )
 
     elif year == "2010-2019":
 
         query = query.filter(
+
             Book.year >= 2010,
+
             Book.year <= 2019
+
         )
 
     elif year == "2020+":
 
         query = query.filter(
+
             Book.year >= 2020
+
         )
 
     # σελιδες
@@ -220,42 +338,59 @@ def books():
     if pages == "1-100":
 
         query = query.filter(
+
             Book.pages >= 1,
+
             Book.pages <= 100
+
         )
 
     elif pages == "101-200":
 
         query = query.filter(
+
             Book.pages >= 101,
+
             Book.pages <= 200
+
         )
 
     elif pages == "201-300":
 
         query = query.filter(
+
             Book.pages >= 201,
+
             Book.pages <= 300
+
         )
 
     elif pages == "301-400":
 
         query = query.filter(
+
             Book.pages >= 301,
+
             Book.pages <= 400
+
         )
 
     elif pages == "401-500":
 
         query = query.filter(
+
             Book.pages >= 401,
+
             Book.pages <= 500
+
         )
 
     elif pages == "500+":
 
         query = query.filter(
+
             Book.pages >= 500
+
         )
 
     # διαθεσιμοτητα
@@ -263,27 +398,38 @@ def books():
     if available_only:
 
         query = query.filter(
+
             Book.status == "Available"
+
         )
 
     books = query.all()
 
     return render_template(
-        "view_books.html",
-        books=books
-    )
 
+        "view_books.html",
+
+        books=books
+
+    )
 
 # στοιχεια βιβλιου
 
 @app.route("/book/<int:id>")
 def book_info(id):
 
-    book = Book.query.get_or_404(id)
+    book = Book.query.get_or_404(
+
+        id
+
+    )
 
     return render_template(
+
         "book_info.html",
+
         book=book
+
     )
 
 
@@ -295,39 +441,62 @@ def book_info(id):
 )
 def edit_book(id):
 
-    book = Book.query.get_or_404(id)
+    book = Book.query.get_or_404(
+
+        id
+
+    )
 
     if request.method == "POST":
 
         new_isbn = request.form["isbn"]
 
         cover_file = request.files.get(
+
             "cover_upload"
+
         )
 
-        cover_url = request.form["cover_url"]
+        cover_url = request.form[
+
+            "cover_url"
+
+        ]
 
         # ελεγχος για νεα εικονα
 
         if cover_file and cover_file.filename != "":
 
             filepath = os.path.join(
+
                 "static",
+
                 "uploads",
+
                 cover_file.filename
+
             )
 
-            cover_file.save(filepath)
+            cover_file.save(
+
+                filepath
+
+            )
 
             cover_url = "/" + filepath.replace(
+
                 "\\",
+
                 "/"
+
             )
 
         # ελεγχος για διπλο isbn
 
         existing = Book.query.filter_by(
+
             isbn=new_isbn
+
         ).first()
 
         if existing and existing.id != book.id:
@@ -353,11 +522,19 @@ def edit_book(id):
         book.cover_type = request.form["cover_type"]
 
         book.description = request.form[
+
             "description"
+
         ]
 
         book.categories = ",".join(
-            request.form.getlist("categories")
+
+            request.form.getlist(
+
+                "categories"
+
+            )
+
         )
 
         book.cover_url = cover_url
@@ -367,12 +544,17 @@ def edit_book(id):
         db.session.commit()
 
         return redirect(
+
             f"/book/{book.id}"
+
         )
 
     return render_template(
+
         "edit_book.html",
+
         book=book
+
     )
 
 
@@ -385,8 +567,12 @@ def edit_book(id):
 def delete_book(id):
 
     book = Book.query.get_or_404(
+
         id
+
     )
+
+    # ελεγχος για ενεργο δανεισμο
 
     active_loan = Loan.query.filter_by(
 
@@ -447,6 +633,7 @@ def delete_book(id):
 
 #------------------- μελη -------------------
 
+
 # λιστα μελων
 
 @app.route("/members")
@@ -461,6 +648,8 @@ def members():
     )
 
     query = Member.query
+
+    # αναζητηση
 
     if search:
 
@@ -510,6 +699,7 @@ def members():
 
     )
 
+
 # πληροφοριες μελους
 
 @app.route(
@@ -518,7 +708,9 @@ def members():
 def member_info(id):
 
     member = Member.query.get_or_404(
+
         id
+
     )
 
     loans = Loan.query.filter_by(
@@ -546,18 +738,22 @@ def member_info(id):
 )
 def add_member():
 
-    # βρισκει το τελευταιο μελος
+    # ευρεση τελευταιου μελους
 
     last_member = Member.query.order_by(
+
         Member.id.desc()
+
     ).first()
 
-    # δημιουργει επομενο αριθμο καρτας
+    # δημιουργια νεου αριθμου καρτας
 
     if last_member:
 
         next_card_number = str(
+
             int(last_member.card_number) + 1
+
         ).zfill(13)
 
     else:
@@ -577,19 +773,30 @@ def add_member():
             phone=request.form["phone"],
 
             date_of_birth=datetime.strptime(
+
                 request.form["date_of_birth"],
+
                 "%Y-%m-%d"
+
             ).date(),
 
             address=request.form["address"]
 
         )
 
-        db.session.add(member)
+        db.session.add(
+
+            member
+
+        )
 
         db.session.commit()
 
-        return redirect("/members")
+        return redirect(
+
+            "/members"
+
+        )
 
     return render_template(
 
@@ -599,6 +806,9 @@ def add_member():
 
     )
 
+
+# επεξεργασια μελους
+
 @app.route(
     "/edit-member/<int:id>",
     methods=["GET", "POST"]
@@ -606,27 +816,37 @@ def add_member():
 def edit_member(id):
 
     member = Member.query.get_or_404(
+
         id
+
     )
 
     if request.method == "POST":
 
         member.first_name = request.form[
+
             "first_name"
+
         ]
 
         member.last_name = request.form[
+
             "last_name"
+
         ]
 
         member.phone = request.form[
+
             "phone"
+
         ]
 
         member.date_of_birth = datetime.strptime(
 
             request.form[
+
                 "date_of_birth"
+
             ],
 
             "%Y-%m-%d"
@@ -634,7 +854,9 @@ def edit_member(id):
         ).date()
 
         member.address = request.form[
+
             "address"
+
         ]
 
         db.session.commit()
@@ -667,14 +889,21 @@ def edit_member(id):
 
     )
 
+
+# διαγραφη μελους
+
 @app.route(
     "/delete-member/<int:id>"
 )
 def delete_member(id):
 
     member = Member.query.get_or_404(
+
         id
+
     )
+
+    # ελεγχος για ενεργους δανεισμους
 
     active_loans = Loan.query.filter_by(
 
@@ -720,34 +949,45 @@ def delete_member(id):
 
     )
 
-# δανεισμος βιβλιου
 
 #------------------- δανεισμοι -------------------
+
+
+# σελιδα δανεισμων
 
 @app.route("/loans")
 def loans():
 
     member_id = request.args.get(
+
         "member_id"
+
     )
 
     book_id = request.args.get(
+
         "book_id"
+
     )
 
     member = None
+
     book = None
 
     if member_id:
 
         member = Member.query.get_or_404(
+
             member_id
+
         )
 
     if book_id:
 
         book = Book.query.get_or_404(
+
             book_id
+
         )
 
     return render_template(
@@ -760,6 +1000,7 @@ def loans():
 
     )
 
+
 # api μελους
 
 @app.route(
@@ -768,37 +1009,49 @@ def loans():
 def api_member(card_number):
 
     member = Member.query.filter_by(
+
         card_number=card_number
+
     ).first()
 
     if not member:
 
         return jsonify(
+
             {
+
                 "found": False
+
             }
+
         )
 
     return jsonify(
 
         {
+
             "found": True,
 
             "first_name":
+
                 member.first_name,
 
             "last_name":
+
                 member.last_name,
 
             "phone":
+
                 member.phone,
 
             "balance":
+
                 member.balance
 
         }
 
     )
+
 
 # api βιβλιου
 
@@ -808,40 +1061,55 @@ def api_member(card_number):
 def api_book(isbn):
 
     book = Book.query.filter_by(
+
         isbn=isbn
+
     ).first()
 
     if not book:
 
         return jsonify(
+
             {
+
                 "found": False
+
             }
+
         )
 
     return jsonify(
 
         {
+
             "found": True,
 
             "title":
+
                 book.title,
 
             "authors":
+
                 book.authors,
 
             "publisher":
+
                 book.publisher,
 
             "year":
+
                 book.year,
 
             "language":
+
                 book.language
 
         }
 
     )
+
+
+# αποθηκευση δανεισμου
 
 @app.route(
     "/save-loan",
@@ -850,6 +1118,8 @@ def api_book(isbn):
 def save_loan():
 
     data = request.get_json()
+
+    # αναζητηση μελους
 
     member = Member.query.filter_by(
 
@@ -870,6 +1140,8 @@ def save_loan():
             }
 
         )
+
+    # ελεγχος οριου δανεισμων
 
     active_loans = Loan.query.filter_by(
 
@@ -900,6 +1172,8 @@ def save_loan():
             }
 
         )
+
+    # δημιουργια δανεισμων
 
     for isbn in data["books"]:
 
@@ -961,6 +1235,12 @@ def save_loan():
 
     )
 
+
+#------------------- επιστροφες -------------------
+
+
+# σελιδα επιστροφων
+
 @app.route(
     "/returns"
 )
@@ -979,6 +1259,8 @@ def returns():
         status="Borrowed"
 
     )
+
+    # αναζητηση
 
     if search:
 
@@ -1036,30 +1318,43 @@ def returns():
 
     )
 
+
+# επιστροφη βιβλιου
+
 @app.route(
     "/return-book/<int:loan_id>"
 )
 def return_book(loan_id):
 
     loan = Loan.query.get_or_404(
+
         loan_id
+
     )
 
     book = Book.query.get(
+
         loan.book_id
+
     )
 
     today = datetime.today().date()
+
+    # ενημερωση δανεισμου
 
     loan.status = "Returned"
 
     loan.return_date = today
 
+    # ενημερωση αντιτυπων
+
     book.copies += 1
 
     book.status = "Available"
 
-    if(
+    # υπολογισμος χρεωσης
+
+    if (
 
         today >
 
@@ -1099,13 +1394,18 @@ def return_book(loan_id):
 
     )
 
+
+# εξοφληση χρεωσης
+
 @app.route(
     "/pay-loan/<int:loan_id>"
 )
 def pay_loan(loan_id):
 
     loan = Loan.query.get_or_404(
+
         loan_id
+
     )
 
     if not loan.paid:
@@ -1132,7 +1432,9 @@ def pay_loan(loan_id):
 
     )
 
-# εκκινηση εφαρμογης
+
+#------------------- εκκινηση εφαρμογης -------------------
+
 
 if __name__ == "__main__":
 
@@ -1140,4 +1442,8 @@ if __name__ == "__main__":
 
         db.create_all()
 
-    app.run(debug=True)
+    app.run(
+
+        debug=True
+
+    )
